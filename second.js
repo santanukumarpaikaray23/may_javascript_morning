38cc6a040332166bc7ab4bb0cef7ee419aa76f78
kumar <santanupaikaray23@gmail.com>
2021-06-18 18:09:52 +0530

    add all changes

---
+++ second.js
@@ -0,0 +1,46 @@
+var a = true
+var b = true
+
+true+true
+2
+true+false
+1
+false+false
+0
+Note:
+true is equal to 1
+false is equal to 0
+
+"a"+true
+"atrue"
+"a"+true+true
+"atruetrue"
+true+true+"a"
+
+var a="10"
+var b="20"
+parseInt(a)+ parseInt(b)
+30
+
+var a = "11a2"
+var b = "12a"
+var c = "a12"
+
+parseInt(a)
+11
+parseInt(b)
+12
+parseInt(c)
+NaN
+
+var a = 10
+var b = "hi"
+var c = true
+
+isNaN(a)(is not a number)
+false
+isNaN(b)
+true
+isNaN(c)
+false
+
