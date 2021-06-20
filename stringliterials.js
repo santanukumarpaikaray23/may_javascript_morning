0d9681e15c0dd8e25fa889e800da9ebdd0bf72c4
kumar <santanupaikaray23@gmail.com>
2021-06-20 08:55:22 +0530

    add all changes

---
+++ stringliterials.js
@@ -0,0 +1,9 @@
+//es5
+var name = "Avengers"
+var rating = 4.5
+var type = "action"
+var ind = "Hollywood"
+
+var out = "The "+name+" is an "+type+" movie,with rating of "+rating+" and from ind "+ind+"."
+//es6
+var out = `The ${name} is an ${type} movie,with rating of ${rating} and from ind ${ind}.`
